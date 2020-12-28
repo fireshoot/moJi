6300cdd5ff2f292b082b6c11f383bf81d2fbd828
yangxin <yangxin@dianmi365.com>
2020-12-28 16:02:53 +0800

    基础框架搭建

---
+++ account-service/src/test/java/com/osyangxin/moji/accountservice/AccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osyangxin.moji.accountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
